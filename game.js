bb46c412d3432a5a276ffabe5268e4771ce970ac
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-08-22 21:32:32 +0900

    player attack & run
    플레이어가 공격하거나 도망가는 것까지 완료
    추가로 스테이지가 넘어가면 몬스터의 피가 올라는 것까지 끝냄

--- game.js
+++ game.js
@@ -1,23 +1,54 @@
 import chalk from 'chalk';
 import readlineSync from 'readline-sync';
 
+function sleep(ms) {
+    const wakeUpTime = Date.now() + ms;
+    while (Date.now() < wakeUpTime) {}
+}
+
 class Player {
-  constructor() {
+  constructor(isRun) {
+    this._isRun = isRun;
     this.hp = 100;
+    this.attack = 5;
   }
 
-  attack() {
+  Attack(logs) {
     // 플레이어의 공격
+    logs.push(chalk.green(`플레이어의 공격! ${this.attack}의 피해를 입혔다!`));
+  }
+
+  Run(logs) {
+    logs.push(chalk.blue(`플레이어는 도망갔다.`));
+    this._isRun = true;
+  }
+
+  set setRun(run) {
+    this._isRun = run;
+  }
+  get getRun() {
+    return this._isRun;
   }
 }
 
 class Monster {
-  constructor() {
-    this.hp = 100;
+  constructor(stage) {
+    this.hp = 15 + ((stage - 1) * 5);
+    this.attack = 5;
   }
 
-  attack() {
+  Attack(logs) {
     // 몬스터의 공격
+    logs.push(chalk.red(`몬스터의 공격! ${this.attack}의 피해를 입었다!`));
+  }
+
+  Damage(logs, playerAttack) {
+    logs.push(chalk.green(`몬스터는 ${playerAttack} 만큼 HP가 깎였다.`));
+    this.hp -= playerAttack;
+
+    if(this.hp <= 0) {
+        logs.push(chalk.blue(`\n몬스터가 사망했다!`));
+    }
   }
 }
 
@@ -25,18 +56,51 @@ function displayStatus(stage, player, monster) {
   console.log(chalk.magentaBright(`\n=== Current Status ===`));
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
-    chalk.blueBright(
-      `| 플레이어 정보`,
-    ) +
-    chalk.redBright(
-      `| 몬스터 정보 |`,
-    ),
+    chalk.blueBright(`| 플레이어 정보 - HP: ${player.hp} ATTACK: ${player.attack} `) +
+    chalk.redBright(`| 몬스터 정보 - HP: ${monster.hp} ATTACK: ${monster.attack} |`)
   );
   console.log(chalk.magentaBright(`=====================\n`));
 }
 
+
+function handleUserInputGame(logs, player, monster) {
+    const choice = readlineSync.question('당신의 선택은? ');
+    logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+
+    switch (choice) {
+        case '1':
+            player.Attack(logs);
+            monster.Damage(logs, player.attack);
+            if(0 < monster.hp) {
+                monster.Attack(logs);
+            }
+            break;
+        case '2':
+            player.Run(logs);
+            break;
+        case '`':
+            console.log(chalk.red('게임을 종료합니다.'));
+            process.exit(0); // 게임 종료
+            break;
+        default:
+            console.log(chalk.red('올바른 선택을 하세요.'));
+            handleUserInputGame(logs, player, monster);
+    }
+}
+
+// 몬스터 행동 지문
+function MonsterAtion(num) {
+    var AtionStr = ['몬스터와 마주쳤다!',
+        '몬스터는 화가 나 보인다.',
+        '몬스터가 위협적으로 포효했다.',
+        '몬스터는 당신을 노려보고 있다.',
+        '몬스터는 공격할 틈을 보고 있는 것 같다.'
+       ]
+    console.log(chalk.green('\n' + AtionStr[num]));
+}
 const battle = async (stage, player, monster) => {
   let logs = [];
+  let count = 0, monsterAtionNum = 0;
 
   while(player.hp > 0) {
     console.clear();
@@ -44,26 +108,38 @@ const battle = async (stage, player, monster) => {
 
     logs.forEach((log) => console.log(log));
 
+    if(player.hp <= 0 || monster.hp <= 0 || player.getRun == true) {
+        sleep(3000);
+        break;
+    }
+    
+    // 몬스터 행동 지문 출력
+    if(count === 0) {
+        count = 1;
+    } else {
+        monsterAtionNum = Math.floor(1 + Math.random() * 4);
+    }
+    MonsterAtion(monsterAtionNum);
     console.log(
       chalk.green(
-        `\n1. 공격한다 2. 아무것도 하지않는다.`,
+        `\n1. 공격 2. 도망.`,
       ),
     );
-    const choice = readlineSync.question('당신의 선택은? ');
 
     // 플레이어의 선택에 따라 다음 행동 처리
-    logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+    handleUserInputGame(logs, player, monster);
   }
-  
+
 };
 
 export async function startGame() {
   console.clear();
-  const player = new Player();
+  const player = new Player(false);
   let stage = 1;
 
   while (stage <= 10) {
     const monster = new Monster(stage);
+    player.setRun = false;
     await battle(stage, player, monster);
 
     // 스테이지 클리어 및 게임 종료 조건
