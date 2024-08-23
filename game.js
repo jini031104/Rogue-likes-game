b6c32f102c5bce1f442d079167fc4dbbe09e340c
jini031104 <90189794+jini031104@users.noreply.github.com>
2024-08-23 17:52:33 +0900

    연속공격 틀, 플레이어 최소 ~ 최대 공격력

--- game.js
+++ game.js
@@ -2,8 +2,8 @@ import chalk from 'chalk';
 import readlineSync from 'readline-sync';
 
 function sleep(ms) {
-    const wakeUpTime = Date.now() + ms;
-    while (Date.now() < wakeUpTime) {}
+  const wakeUpTime = Date.now() + ms;
+  while (Date.now() < wakeUpTime) { }
 }
 
 class Player {
@@ -12,12 +12,26 @@ class Player {
     this._maxHP = maxHP
     this.hp = 100;
     this.recoveryHp = 5;
-    this.attack = 5;
+    this.minAttack = 5;
+    this.maxAttack = 10;
+    this.attack = 0;
+    this.bubbleAttackSuccess = 25;
   }
 
   Attack(logs) {
     // 플레이어의 공격
+    // 최소 ~ 최대 사이의 공격을 가한다.
+    this.attack = Math.floor(Math.random() * (this.maxAttack - this.minAttack + 1)) + this.minAttack;
     logs.push(chalk.green(`플레이어의 공격! ${this.attack}의 피해를 입혔다!`));
+  }
+
+  DublleAttack(logs) {
+    let success = Math.floor(Math.random() * 100); // 0 ~ 99
+    if(success <= this.bubbleAttackSuccess) {
+      logs.push(chalk.green(`플레이어의 연속 공격!!!!`));
+    } else {
+      logs.push(chalk.green(`연속 공격 실패...`));
+    }
   }
 
   Run(logs) {
@@ -30,11 +44,13 @@ class Player {
     this.hp -= monsterAttack;
   }
 
-  Recovery() {
-    if(0 < this.hp) {
+  RecoveryAndAttack() {
+    if (0 < this.hp) {
       this.hp += this.hp < this.getMaxHP ? this.recoveryHp : 0;
-      if(this.getRun === false) // 도망가지 않았을 때만 공격력 상승
-        this.attack += 2;
+      if (this.getRun === false) {  // 도망가지 않았을 때만 공격력 상승
+        this.minAttack += 2;
+        this.maxAttack += 3;
+      }
     }
   }
 
@@ -64,8 +80,8 @@ class Monster {
     logs.push(chalk.green(`몬스터는 ${playerAttack} 만큼 HP가 깎였다.`));
     this.hp -= playerAttack;
 
-    if(this.hp <= 0)
-        logs.push(chalk.blue(`\n몬스터가 사망했다!`));
+    if (this.hp <= 0)
+      logs.push(chalk.blue(`\n몬스터가 사망했다!`));
   }
 }
 
@@ -74,81 +90,86 @@ function displayStatus(stage, player, monster) {
   console.log(chalk.magentaBright(`\n=== Current Status ===`));
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
-    chalk.blueBright(`| 플레이어 정보 - HP: ${player.hp} ATTACK: ${player.attack} `) +
+    chalk.blueBright(`| 플레이어 정보 - HP: ${player.hp} ATTACK: ${player.minAttack}~${player.maxAttack} `) +
     chalk.redBright(`| 몬스터 정보 - HP: ${monster.hp} ATTACK: ${monster.attack} |`)
   );
   console.log(chalk.magentaBright(`=====================\n`));
 }
 
 function handleUserInputGame(logs, player, monster) {
-    const choice = readlineSync.question('당신의 선택은? ');
-    logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+  const choice = readlineSync.question('당신의 선택은? ');
+  logs.push(chalk.green(`\n${choice}를 선택하셨습니다.`));
 
-    switch (choice) {
-        case '1':
-            player.Attack(logs);
-            monster.Damage(logs, player.attack);
-            if(0 < monster.hp) {
-                monster.Attack(logs);
-                player.Damage(logs, monster.attack);
-            }
-            break;
-        case '2':
-            player.Run(logs);
-            break;
-        case '`': // 개발자 키
-            console.log(chalk.red('게임을 종료합니다.'));
-            process.exit(0); // 게임 종료
-            break;
-        default:
-            console.log(chalk.red('올바른 선택을 하세요.'));
-            handleUserInputGame(logs, player, monster);
-    }
+  switch (choice) {
+    case '1': // 일반 공격
+      player.Attack(logs);
+      monster.Damage(logs, player.attack);
+      if (0 < monster.hp) {
+        monster.Attack(logs);
+        player.Damage(logs, monster.attack);
+      }
+      break;
+    case '2': // 연속 공격
+      player.DublleAttack(logs);
+      break;
+    case '3': // 방어
+      break;
+    case '4': // 도망
+      player.Run(logs);
+      break;
+    case '`': // 개발자 키
+      console.log(chalk.red('게임을 종료합니다.'));
+      process.exit(0); // 게임 종료
+      break;
+    default:
+      console.log(chalk.red('올바른 선택을 하세요.'));
+      handleUserInputGame(logs, player, monster);
+  }
 }
 
 // 몬스터 행동 지문
 function MonsterAtion(num) {
-    var AtionStr = ['몬스터와 마주쳤다!',
-        '몬스터는 화가 나 보인다.',
-        '몬스터가 위협적으로 포효했다.',
-        '몬스터는 당신을 노려보고 있다.',
-        '몬스터는 공격할 틈을 보고 있는 것 같다.'
-       ]
-    console.log(chalk.green('\n' + AtionStr[num]));
+  var AtionStr = ['몬스터와 마주쳤다!',
+    '몬스터는 화가 나 보인다.',
+    '몬스터가 위협적으로 포효했다.',
+    '몬스터는 당신을 노려보고 있다.',
+    '몬스터는 공격할 틈을 보고 있는 것 같다.'
+  ]
+  console.log(chalk.green('\n' + AtionStr[num]));
 }
 const battle = async (stage, player, monster) => {
   let logs = [];
   let count = 0, monsterAtionNum = 0;
 
-  while(player.hp > 0) {
+  while (player.hp > 0) {
     displayStatus(stage, player, monster);
-
     logs.forEach((log) => console.log(log));
 
-    if(player.hp <= 0 || monster.hp <= 0 || player.getRun == true) {
+    // 배틀 종료
+    if (player.hp <= 0 || monster.hp <= 0 || player.getRun == true) {
       console.log(chalk.yellow(`\n다음 스테이지 준비 중...`));
       sleep(3000);
       break;
     }
-    
+
     // 몬스터 행동 지문 출력
     count === 0 ? count = 1 : monsterAtionNum = Math.floor(1 + Math.random() * 4);
     MonsterAtion(monsterAtionNum);
-    console.log(chalk.green(`\n1. 공격 2. 도망.`));
+    console.log(chalk.green(`\n1. 공격  2. 연속 공격(${player.bubbleAttackSuccess}%)  3. 방어  4. 도망`));
 
     // 플레이어의 선택에 따라 다음 행동 처리
     handleUserInputGame(logs, player, monster);
   }
 
   // 플레이어 사망
-  if(player.hp <= 0) {
+  if (player.hp <= 0) {
     displayStatus(stage, player, monster);
     logs.forEach((log) => console.log(log));
     console.log(chalk.red(`\n플레이어는 사망했다...`));
     sleep(3000);
   }
   // 배틀이 끝나면 플레이어 회복 & 공격력 상승
-  player.Recovery();
+  player.RecoveryAndAttack();
 };
 
 export async function startGame() {
@@ -162,7 +183,7 @@ export async function startGame() {
     await battle(stage, player, monster);
 
     // 스테이지 클리어 및 게임 종료 조건
-    if(player.hp <= 0)
+    if (player.hp <= 0)
       break;
 
     stage++;
